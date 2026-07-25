5908449eab2e41477dbcf7cee3672acb8a306251
FRCTeam3255-Shared <170778697+FRCTeam3255-Shared-K1-10@users.noreply.github.com>
2026-07-25 16:29:36 -0700

    3-create-empty-state-commands

--- src/main/java/frc/robot/commands/states/presetLocations/PreppingOpposingToAlliance.java
+++ src/main/java/frc/robot/commands/states/presetLocations/PreppingOpposingToAlliance.java
@@ -2,28 +2,31 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.commands.states;
+package frc.robot.commands.states.presetLocations;
 
 import edu.wpi.first.wpilibj2.command.Command;
 
 /* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
-public class PreppingClimb extends Command {
-  /** Creates a new PreppingClimb. */
-  public PreppingClimb() {
+public class PreppingOpposingToAlliance extends Command {
+  /** Creates a new PreppingOpposingToAlliance. */
+  public PreppingOpposingToAlliance() {
     // Use addRequirements() here to declare subsystem dependencies.
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+  }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+  }
 
   // Returns true when the command should end.
   @Override
