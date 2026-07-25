e1cf8f07aef965ba73f38ceb181be4419b1ec4a6
FRCTeam3255-Shared <170778697+FRCTeam3255-Shared-K1-10@users.noreply.github.com>
2026-07-25 15:54:48 -0700

    3-create-empty-state-commands

--- src/main/java/frc/robot/commands/states/PreppingClimb.java
+++ src/main/java/frc/robot/commands/states/PreppingClimb.java
@@ -7,9 +7,9 @@ package frc.robot.commands.states;
 import edu.wpi.first.wpilibj2.command.Command;
 
 /* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
-public class None extends Command {
-  /** Creates a new None. */
-  public None() {
+public class PreppingClimb extends Command {
+  /** Creates a new PreppingClimb. */
+  public PreppingClimb() {
     // Use addRequirements() here to declare subsystem dependencies.
   }
 
