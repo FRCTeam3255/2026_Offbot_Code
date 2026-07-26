cee65edd5414108e66269e63eb786aaedf38dde4
Yutong Zhu <yutong59202@gmail.com>
2026-07-25 20:30:37 -0700

    Added logic for robot poses (づ｡◕‿‿◕｡)づ (#22)
    * im so done
    added logic for robot poses
    
    * qf, i guess
    
    * qf, again
    
    * qf, final drafe, trust

--- src/main/java/frc/robot/subsystems/RobotPoses.java
+++ src/main/java/frc/robot/subsystems/RobotPoses.java
@@ -6,6 +6,9 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.epilogue.Logged;
 import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.units.Units;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.FieldObject2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -19,18 +22,48 @@ public class RobotPoses extends SubsystemBase {
   FieldObject2d robotObject = field2d.getObject("Robot");
 
   Pose3d modelDrivetrain = Pose3d.kZero;
+  Pose3d model0Intake = Pose3d.kZero;
+  Pose3d model1Turret = Pose3d.kZero;
+  Pose3d model2Hood = Pose3d.kZero;
+
+  Transform3d intakeTransform3d = new Transform3d();
+  Rotation3d turretRotation3d = Rotation3d.kZero;
+  Rotation3d hoodRotation3d = Rotation3d.kZero;
+
+  Transform3d turretPivotPoint = Transform3d.kZero;
+  Transform3d hoodPivotPoint = Transform3d.kZero;
 
   public RobotPoses() {
-
     SmartDashboard.putData("Field", field2d);
   }
 
   @Override
   public void periodic() {
+    intakeTransform3d = new Transform3d(
+        Units.Inches.zero(),
+        Units.Inches.zero(),
+        RobotContainer.positionalInstance.lastDesiredIntakePosition,
+        Rotation3d.kZero);
+
+    turretRotation3d = new Rotation3d(
+        Units.Degrees.zero(),
+        Units.Degrees.zero(),
+        RobotContainer.positionalInstance.lastDesiredTurretAngle);
+
+    hoodRotation3d = new Rotation3d(
+        Units.Degrees.zero(),
+        RobotContainer.positionalInstance.lastDesiredHoodPivotAngle,
+        Units.Degrees.zero());
+
     robotObject.setPose(RobotContainer.drivetrainInstance.getPose());
     // This method will be called once per scheduler run
 
     // Robot Positions
     modelDrivetrain = new Pose3d(RobotContainer.drivetrainInstance.getPose());
+    model0Intake = Pose3d.kZero.transformBy(intakeTransform3d);
+    model1Turret = Pose3d.kZero.rotateAround(
+        Pose3d.kZero.plus(turretPivotPoint).getTranslation(), turretRotation3d);
+    model2Hood = Pose3d.kZero
+        .rotateAround(Pose3d.kZero.plus(hoodPivotPoint).getTranslation(), hoodRotation3d);
   }
 }
