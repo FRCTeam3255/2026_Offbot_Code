cdb3186ea7cfad375f5be9b0ee312fcf5c874c30
FRCTeam3255-Shared-K1-10 <frcteam3255@gmail.com>
2026-08-01 11:19:24 -0500

    created motors with set and get functions (#20)
    * we created the motors, added set and get functions.
    
    Co-Authored-By: Tej shah <187054380+Tejshah88@users.noreply.github.com>
    Co-Authored-By: Justinpham17386769 <244249272+Justinpham17386769@users.noreply.github.com>
    
    * updated motor names
    
    Co-Authored-By: Tej shah <187054380+Tejshah88@users.noreply.github.com>
    Co-Authored-By: Justinpham17386769 <244249272+Justinpham17386769@users.noreply.github.com>
    
    * changed aligned to opposed
    
    Co-Authored-By: Tej shah <187054380+Tejshah88@users.noreply.github.com>
    Co-Authored-By: Justinpham17386769 <244249272+Justinpham17386769@users.noreply.github.com>
    
    * fixxed names and updated terms
    
    Co-Authored-By: Tej shah <187054380+Tejshah88@users.noreply.github.com>
    Co-Authored-By: Justinpham17386769 <244249272+Justinpham17386769@users.noreply.github.com>
    
    * changed getTransferBeltSpeed func to getFlywheelSpeed
    
    Co-Authored-By: Tej shah <187054380+Tejshah88@users.noreply.github.com>
    Co-Authored-By: Justinpham17386769 <244249272+Justinpham17386769@users.noreply.github.com>
    
    * renamed variables, changed follower namings
    
    ---------
    
    Co-authored-by: FRCTeam3255-Shared <170778697+FRCTeam3255-Shared@users.noreply.github.com>
    Co-authored-by: Tej shah <187054380+Tejshah88@users.noreply.github.com>
    Co-authored-by: Justinpham17386769 <244249272+Justinpham17386769@users.noreply.github.com>

--- src/main/java/frc/robot/constants/ConstFreeSpin.java
+++ src/main/java/frc/robot/constants/ConstFreeSpin.java
@@ -4,9 +4,21 @@
 
 package frc.robot.constants;
 
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+
 /** Add your docs here. */
 public class ConstFreeSpin {
+
   public static final double STOP = 0;
+
+  public static final TalonFXConfiguration INTAKE_ROLLERS_WEST_CONFIGURATION = new TalonFXConfiguration();
+  public static final TalonFXConfiguration INTAKE_ROLLERS_EAST_CONFIGURATION = new TalonFXConfiguration();
+  public static final TalonFXConfiguration HOTDOG_ROLLERS_CONFIGURATION = new TalonFXConfiguration();
+  public static final TalonFXConfiguration TRANSFER_BELT_CONFIGURATION = new TalonFXConfiguration();
+  public static final TalonFXConfiguration FLYWHEEL_WEST_CONFIGURATION = new TalonFXConfiguration();
+  public static final TalonFXConfiguration FLYWHEEL_EAST_CONFIGURATION = new TalonFXConfiguration();
+  public static final TalonFXConfiguration AGITATOR_CONFIGURATION = new TalonFXConfiguration();
+  public static final TalonFXConfiguration TRANSFER_RAMP_CONFIGURATION = new TalonFXConfiguration();
 
   static {
     // Configure TalonFXConfiguration objects here
--- src/main/java/frc/robot/subsystems/FreeSpin.java
+++ src/main/java/frc/robot/subsystems/FreeSpin.java
@@ -4,12 +4,103 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.controls.Follower;
+import com.ctre.phoenix6.controls.MotionMagicVelocityVoltage;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.MotorAlignmentValue;
+
 import edu.wpi.first.epilogue.Logged;
+import edu.wpi.first.units.measure.AngularVelocity;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.DeviceIDs;
+import frc.robot.constants.ConstFreeSpin;
 
 @Logged
 public class FreeSpin extends SubsystemBase {
+  final TalonFX intakeRollerWestLeader = new TalonFX(DeviceIDs.freeSpinIDs.INTAKE_ROLLERS_WEST_CAN);
+  final TalonFX intakeRollerEastFollower = new TalonFX(DeviceIDs.freeSpinIDs.INTAKE_ROLLERS_EAST_CAN);
+
+  final TalonFX hotdogRollers = new TalonFX(DeviceIDs.freeSpinIDs.HOTDOG_ROLLERS_CAN);
+  final TalonFX transferBelt = new TalonFX(DeviceIDs.freeSpinIDs.TRANSFER_BELT_CAN);
+
+  final TalonFX westFlywheelLeader = new TalonFX(DeviceIDs.freeSpinIDs.FLYWHEEL_WEST_CAN);
+  final TalonFX eastFlywheelFollower = new TalonFX(DeviceIDs.freeSpinIDs.FLYWHEEL_EAST_CAN);
+
+  final TalonFX agitator = new TalonFX(DeviceIDs.freeSpinIDs.AGITATOR_CAN);
+  final TalonFX transferRamp = new TalonFX(DeviceIDs.freeSpinIDs.TRANSFER_RAMP_CAN);
+
+  Follower flywheelFollower = new Follower(westFlywheelLeader.getDeviceID(), MotorAlignmentValue.Opposed);
+
+  Follower intakeFollower = new Follower(intakeRollerWestLeader.getDeviceID(), MotorAlignmentValue.Opposed);
+
   public FreeSpin() {
+
+    intakeRollerWestLeader.getConfigurator().apply(ConstFreeSpin.INTAKE_ROLLERS_WEST_CONFIGURATION);
+    intakeRollerEastFollower.getConfigurator().apply(ConstFreeSpin.INTAKE_ROLLERS_EAST_CONFIGURATION);
+    hotdogRollers.getConfigurator().apply(ConstFreeSpin.HOTDOG_ROLLERS_CONFIGURATION);
+    transferBelt.getConfigurator().apply(ConstFreeSpin.TRANSFER_BELT_CONFIGURATION);
+    westFlywheelLeader.getConfigurator().apply(ConstFreeSpin.FLYWHEEL_WEST_CONFIGURATION);
+    eastFlywheelFollower.getConfigurator().apply(ConstFreeSpin.FLYWHEEL_EAST_CONFIGURATION);
+    agitator.getConfigurator().apply(ConstFreeSpin.AGITATOR_CONFIGURATION);
+    transferRamp.getConfigurator().apply(ConstFreeSpin.TRANSFER_RAMP_CONFIGURATION);
+
+  }
+
+  final MotionMagicVelocityVoltage flywheelVelocityRequest = new MotionMagicVelocityVoltage(0);
+
+  public void setIntakeRollersPercentOutput(double percentOutput) {
+    intakeRollerWestLeader.set(percentOutput);
+    intakeRollerEastFollower.setControl(intakeFollower);
+  }
+
+  public AngularVelocity getIntakeRollersVelocity() {
+    return intakeRollerWestLeader.getVelocity().getValue();
+  }
+
+  public void setFlywheelVelocity(AngularVelocity velocity) {
+    westFlywheelLeader.setControl(flywheelVelocityRequest.withVelocity(velocity));
+    eastFlywheelFollower.setControl(flywheelFollower);
+  }
+
+  public void setFlywheelPercentOutput(double percentOutput) {
+    westFlywheelLeader.set(percentOutput);
+    eastFlywheelFollower.setControl(flywheelFollower);
+  }
+
+  public AngularVelocity getFlywheelVelocity() {
+    return westFlywheelLeader.getVelocity().getValue();
+  }
+
+  public void setHotdogRollersPercentOutput(double percentOutput) {
+    hotdogRollers.set(percentOutput);
+  }
+
+  public AngularVelocity getHotdogRollersVelocity() {
+    return hotdogRollers.getVelocity().getValue();
+  }
+
+  public void setTransferBeltPercentOutput(double percentOutput) {
+    transferBelt.set(percentOutput);
+  }
+
+  public AngularVelocity getTransferBeltVelocity() {
+    return transferBelt.getVelocity().getValue();
+  }
+
+  public void setAgitatorPercentOutput(double percentOutput) {
+    agitator.set(percentOutput);
+  }
+
+  public AngularVelocity getAgitatorVelocity() {
+    return agitator.getVelocity().getValue();
+  }
+
+  public void setTransferRampPercentOutput(double percentOutput) {
+    transferRamp.set(percentOutput);
+  }
+
+  public AngularVelocity getTransferRampVelocity() {
+    return transferRamp.getVelocity().getValue();
   }
 
   @Override
