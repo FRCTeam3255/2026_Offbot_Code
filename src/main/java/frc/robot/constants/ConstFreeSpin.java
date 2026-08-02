2f72f2066df16366feae9166632fc3fe5f281ff4
BrodyKarr <boocat1030@icloud.com>
2026-08-01 18:50:08 -0700

    Update ConstFreeSpin.java
    Co-Authored-By: Tej shah <187054380+Tejshah88@users.noreply.github.com>

--- src/main/java/frc/robot/constants/ConstFreeSpin.java
+++ src/main/java/frc/robot/constants/ConstFreeSpin.java
@@ -19,8 +19,8 @@ public class ConstFreeSpin {
   public static final double REVERSE_AGITATOR_PERCENT_OUTPUT = -1;
   public static final AngularVelocity FLYWHEEL_TOLERANCE = RPM.of(100);
 
-  public static final AngularVelocity TRANFER_BELT_PERCENT_VELOCITY = RPM.of(1);
-  public static final AngularVelocity TRANFER_RAMP_PERCENT_VELOCITY = RPM.of(1);
+  public static final AngularVelocity TRANSFER_BELT_PERCENT_VELOCITY = RPM.of(1);
+  public static final AngularVelocity TRANSFER_RAMP_PERCENT_VELOCITY = RPM.of(1);
   public static final AngularVelocity HOTDOG_ROLLERS_PERCENT_VELOCITY = RPM.of(1);
   public static final AngularVelocity AGITATOR_PERCENT_VELOCITY = RPM.of(1);
   public static final AngularVelocity PREP_D_SIDE_FLYWHEEL = RPM.of(100);// TODO: find actual value
