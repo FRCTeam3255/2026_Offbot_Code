0add5b97e166ec6d9629b0e181f16aa8e37ac05c
FRCTeam3255-Shared <frcteam3255@gmail.com>
2026-08-07 18:31:39 -0700

    62-improve-constants

--- src/main/java/frc/robot/constants/ConstFreeSpin.java
+++ src/main/java/frc/robot/constants/ConstFreeSpin.java
@@ -17,10 +17,10 @@ public class ConstFreeSpin {
   public static final double REVERSING_FLYWHEEL_PERCENT_OUTPUT = -1;
   public static final double REVERSING_TRANSFER_BELT_PERCENT_OUTPUT = -1;
   public static final double REVERSING_TRANSFER_RAMP_PERCENT_OUTPUT = -1;
-  public static final double REVERSE_AGITATOR_PERCENT_OUTPUT = -1;
-  public static final double REVERSE_HOTDOG_ROLLERS_PERCENT_OUTPUT = -1;
+  public static final double REVERSING_AGITATOR_PERCENT_OUTPUT = -1;
+  public static final double REVERSING_HOTDOG_ROLLERS_PERCENT_OUTPUT = -1;
   public static final double INTAKE_ROLLER_PERCENT_OUTPUT = 1;
-  public static final double REVERSE_INTAKE_PERCENT_OUTPUT = -1;
+  public static final double REVERSING_INTAKE_PERCENT_OUTPUT = -1;
 
   public static final AngularVelocity FLYWHEEL_TOLERANCE = RPM.of(100);
 
@@ -28,7 +28,6 @@ public class ConstFreeSpin {
   public static final AngularVelocity TRANSFER_RAMP_VELOCITY = RPM.of(6000);// TODO: replace with actual values
   public static final AngularVelocity AGITATOR_VELOCITY = RPM.of(6000);// TODO: replace with actual values
   public static final AngularVelocity HOTDOG_ROLLERS_VELOCITY = RPM.of(6000);// TODO: replace with actual values
-  
 
   public static final AngularVelocity PREP_D_SIDE_FLYWHEEL = RPM.of(4100);// TODO: find actual value
   public static final AngularVelocity PREP_O_SIDE_FLYWHEEL = RPM.of(4100);// TODO: find actual value
@@ -38,7 +37,6 @@ public class ConstFreeSpin {
   public static final AngularVelocity PREP_TOWER_FLYWHEEL = RPM.of(4300);// TODO: find actual value
   public static final AngularVelocity PREP_TRENCH_FLYWHEEL = RPM.of(4100);// TODO:find actual value
 
-
   public static final TalonFXConfiguration FLYWHEEL_WEST_CONFIGURATION = new TalonFXConfiguration();
   public static final TalonFXConfiguration FLYWHEEL_EAST_CONFIGURATION = new TalonFXConfiguration();
   public static final TalonFXConfiguration TRANSFER_RAMP_CONFIGURATION = new TalonFXConfiguration();
@@ -47,8 +45,6 @@ public class ConstFreeSpin {
   public static final TalonFXConfiguration HOTDOG_ROLLERS_CONFIGURATION = new TalonFXConfiguration();
   public static final TalonFXConfiguration INTAKE_ROLLERS_WEST_CONFIGURATION = new TalonFXConfiguration();
   public static final TalonFXConfiguration INTAKE_ROLLERS_EAST_CONFIGURATION = new TalonFXConfiguration();
-  
-  
 
   static {
     // Configure TalonFXConfiguration objects here
