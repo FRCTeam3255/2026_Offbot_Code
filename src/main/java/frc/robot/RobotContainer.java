fdbab2da6c46c457ebd3f301fe126b917e3277af
BrodyKarr <boocat1030@icloud.com>
2026-08-15 14:36:57 -0700

    Update RobotContainer.java

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -231,6 +231,11 @@ public class RobotContainer {
 
     Command DoNothing = Commands.none();
 
+    Command PreloadThenBackUp = Commands.sequence(
+        ShootingOnMove(
+            ChoreoTraj.PreloadAndBackupTower));
+    autoStartingPoses.put(PreloadThenBackUp, ChoreoTraj.PreloadAndBackupTower);
+
     Command DSideNeutral = Commands.sequence(
         IntakeAndShootOnTheFly(ChoreoTraj.DSideTrenchToNeutral,
             ChoreoTraj.FirstDSideNeutralToAlliance,
@@ -303,6 +308,7 @@ public class RobotContainer {
     // Add more autonomous routines as needed, e.g.:\
     // autoChooser.addOption("Score and Leave", runPath("ScoreAndLeave"));
     autoChooser.setDefaultOption("Do Nothing", DoNothing);
+    autoChooser.addOption("PreloadThenBackUp", PreloadThenBackUp);
     autoChooser.addOption("AutoPIDTuning", AutoPIDTuning);
     autoChooser.addOption("DSideNeutral", DSideNeutral);
     autoChooser.addOption("DSideDoubleNeutral", DSideDoubleNeutral);
