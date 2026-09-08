bb6415fb252e84ee053e8c922b0816dfa7060a0c
Yutong Zhu <yutong59202@gmail.com>
2026-09-07 21:18:12 -0700

    Turret StateMachine (#114)

---
+++ src/main/java/frc/robot/commands/states/turretStates/TurretPreppingDSide.java
@@ -0,0 +1,15 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.states.turretStates;
+
+import frc.robot.constants.ConstPositional;
+import frc.robot.subsystems.TurretStateMachine.TurretState;
+
+/* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
+public class TurretPreppingDSide extends TurretBasePrep {
+  public TurretPreppingDSide() {
+    super(ConstPositional.PREP_D_SIDE_TURRET, TurretState.PREPPING_DSIDE);
+  }
+}
